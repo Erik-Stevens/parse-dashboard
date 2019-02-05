2727b3dbef5c39442fa7ef4c2415bed12707ce93
Erik-Stevens <erikstevens@gmail.com>
2019-02-04 15:56:28 -0900

    Update index.js

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ var app = express();
 // make the Parse Dashboard available at /
 app.use('/', dashboard);
 
-var port = process.env.PORT || 4040;
+var port = 4040;
 var httpServer = require('http').createServer(app);
 httpServer.listen(port, function() {
   console.log('parse-dashboard-example running on port ' + port + '.');
